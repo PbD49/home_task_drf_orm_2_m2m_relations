24cc59354a096c2dfb565c9a47b8554bb7d9b283
Aleksey49 <bezfamilii49@mail.ru>
2024-08-18 02:12:44 +0300

    Initial commit

---
+++ website/articles/views.py
@@ -0,0 +1,20 @@
+from django.db.models import Prefetch
+from django.shortcuts import render
+
+from .models import Article, Scope
+
+
+def articles_list(request):
+    template = 'news.html'
+    ordering = '-published_at'
+    objects_list = Article.objects.order_by(ordering).prefetch_related(
+        Prefetch(
+            'scopes',
+            queryset=Scope.objects.order_by('-is_main', 'tag__name').select_related('tag'))
+    )
+    context = {"object_list": objects_list}
+
+    # используйте этот параметр для упорядочивания результатов
+    # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+
+    return render(request, template, context)
